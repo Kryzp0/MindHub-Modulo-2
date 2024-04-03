31e50fb178e5d674a9491ae5c85e92dd61de4d21
Kryzp0 <chris.jeffry.vega@gmail.com>
2024-04-03 17:58:31 -0300

    se optimiza renderización de cartas con template y se crea nueva función para acortar texto

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
 let container = document.getElementById("container")
 
-let createCard = array => {
-    let muchoTexto
-    if (array.overview.length>230) {
-        muchoTexto = `${array.overview.slice(0,230)}...`
-    } else {
-        muchoTexto = array.overview
-    }
-
-    return `<div class="h-[430px] w-[320px] bg-[#adadad] rounded-3xl p-4">
+let createCard = array => `<div class="h-[430px] w-[320px] bg-[#adadad] rounded-3xl p-4">
     <img class="h-[50%] object-cover" src="${array.image}" alt="${array.title}">
     <div>
         <h3 class="font-semibold text-lg">${array.title}</h3>
         <h4 class="italic font-light">${array.tagline}</h4>
-        <p class="text-sm">${muchoTexto}</p>
+        <p class="text-sm">${cortarTexto(array.overview)}</p>
     </div>
     </div>`
+
+
+let cortarTexto = texto => {
+    if (texto.length>230) {
+       return`${texto.slice(0,230)}...`
+    } else {
+       return texto
+    }
 }
 
 let renderCard = (array,contenedor) => {
+    let template = ''
     for (const iterator of array) {
-        contenedor.innerHTML += createCard(iterator)
+        template += createCard(iterator)
     }
+    contenedor.innerHTML = template
 }
 
 renderCard(movies,container)
