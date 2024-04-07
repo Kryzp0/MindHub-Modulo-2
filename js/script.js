f86b89c24beef50dfc4120803271f4c02aa41856
Kryzp0 <chris.jeffry.vega@gmail.com>
2024-04-07 18:04:34 -0400

    Se agregan filtros por checkbox y texto en pagina Movies

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let container = document.getElementById("container")
+let contenedorCheckboxs = document.getElementById("checkboxs")
+let inputTexto = document.getElementById("search")
 
 let createCard = array => `<div class="h-[430px] w-[320px] bg-[#adadad] rounded-3xl p-4">
     <img class="h-[50%] object-cover" src="${array.image}" alt="${array.title}">
@@ -11,19 +13,64 @@ let createCard = array => `<div class="h-[430px] w-[320px] bg-[#adadad] rounded-
 
 
 let cortarTexto = texto => {
-    if (texto.length>230) {
-       return`${texto.slice(0,230)}...`
+    if (texto.length > 170) {
+        return `${texto.slice(0, 170)}...`
     } else {
-       return texto
+        return texto
     }
 }
 
-let renderCard = (array,contenedor) => {
+let renderCard = (array, contenedor) => {
     let template = ''
-    for (const iterator of array) {
-        template += createCard(iterator)
+    if (array.length != 0) {
+        for (const iterator of array) {
+            template += createCard(iterator)
+        }
+    } else {
+        template = '<h2 class="text-white">Sorry, no movies match your filters. Please try adjusting your selections or search criteria.</h2>'
     }
     contenedor.innerHTML = template
 }
 
-renderCard(movies,container)
+// Se guarda en un nuevo array plano cada propiedad genres, debe ser plano para asegurar que solo sea un array y poder guardar en un Set evitando que se repita cada genero
+let generos = array => new Set(array.flatMap(e => e.genres))
+
+
+let crearCheckbox = nombre => `<label>${nombre}
+<input type="checkbox" name="${nombre}" value="${nombre}">
+</label>`
+
+let renderCheckbox = (array, contenedor) => {
+    let template = ''
+    array.forEach(element => { template += crearCheckbox(element) });
+    contenedor.innerHTML = template
+}
+
+renderCheckbox(generos(movies), contenedorCheckboxs)
+
+renderCard(movies, container)
+
+let generosSeleccionados = []
+let textoIngresado = ''
+contenedorCheckboxs.addEventListener("change", (e) => {
+    // Se agrega al array el valor de todos los input de tipo checkbox que estén checkeados
+    generosSeleccionados = [...document.querySelectorAll('input[type="checkbox"]:checked')].map(input => input.value)
+    renderCard(peliculasPorTexto(peliculasPorCheck(movies, generosSeleccionados), textoIngresado), container)
+})
+inputTexto.addEventListener("input", (e) => {
+    textoIngresado = e.target.value.trim().toLowerCase()
+    console.log(textoIngresado);
+    renderCard(peliculasPorTexto(peliculasPorCheck(movies, generosSeleccionados), textoIngresado), container)
+})
+
+let peliculasPorCheck = (array, arrayChecks) => {
+    if (generosSeleccionados.length == 0) {
+        return array
+    } else {
+        // Con includes se verifica si generos seleccionados incluye el algun genero del array dentro de la propiedad genres del objeto[0] dentro de movies
+        // Con some se itera en el array genres para usar cada elemento como parametro en el metodo includes
+        return array.filter(pelicula => pelicula.genres.some(genero => arrayChecks.includes(genero)))
+    }
+}
+let peliculasPorTexto = (array, textoIngresado) => array.filter(pelicula => pelicula.title.toLocaleLowerCase().includes(textoIngresado))
+
